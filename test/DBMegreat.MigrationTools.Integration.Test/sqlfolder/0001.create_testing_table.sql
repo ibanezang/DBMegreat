364f2f95dbefa3abef4af8e804c37426796ba58f
Anang Satria <anang.dista.satria@gmail.com>
2021-02-01 21:58:22 +0700

    Update to .NET 5.0

--- test/DBMegreat.MigrationTools.Integration.Test/sqlfolder/0001.create_testing_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE test_table (
-  test_table_id int(11) NOT NULL,
-  name varchar(100) NOT NULL,
-  created_datetime datetime NOT NULL,
-  PRIMARY KEY (test_table_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
